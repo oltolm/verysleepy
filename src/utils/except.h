6ee148df8fc8b33528365b04e1d6190a3cd4230e
oltolm <oleg.tolmatcev@gmail.com>
2025-12-16 01:36:13 +0100

    use Unicode conversion functions in SleepyException

--- src/utils/except.h
+++ src/utils/except.h
@@ -25,31 +25,30 @@ http://www.gnu.org/copyleft/gpl.html..
 
 #include <stdexcept>
 #include <string>
+#include "stringutils.h"
 
 class SleepyException : public std::runtime_error
 {
 	std::wstring _what;
 
-	std::string helper(const wchar_t *what)
+public:
+	SleepyException(const std::string& what)
+		: std::runtime_error(what),
+		  _what(utf8ToWide(what))
 	{
-		// Need a temporary to build std::string.
-		// Can't use _what in initialization-list because
-		// it will always be initialized after the superclass.
-		// Can't use _what here because it is still not initialized
-		// and contains junk.
-		std::wstring ws(what);
-		return std::string(ws.begin(), ws.end());
 	}
 
-public:
-	SleepyException(const std::string &what)
-		: std::runtime_error(what), _what(std::wstring(what.begin(), what.end())) {}
-
-	SleepyException(const std::wstring &what)
-		: std::runtime_error(std::string(what.begin(), what.end())), _what(what) {}
+	SleepyException(const std::wstring& what)
+		: std::runtime_error(wideToUtf8(what)),
+		  _what(what)
+	{
+	}
 
 	SleepyException(const wchar_t *what)
-		: std::runtime_error(helper(what)), _what(what) {}
+		: std::runtime_error(wideToUtf8(what)),
+		  _what(what)
+	{
+	}
 
 	const std::wstring &wwhat() const { return _what; }
 };
